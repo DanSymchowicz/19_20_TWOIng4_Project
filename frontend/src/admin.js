8fa57f8a3cc3d344a7d0a323c5e221e897dde9e2
JadouECE <jade.laville@edu.ece.fr>
2019-11-26 15:14:55 +0100

    Premiere version fonctionnelle input user

---
+++ frontend/src/admin.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Admin extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            value:"",
+            taille:"",
+            nb:0
+        }
+    }
+
+    handleChange_nom = (event) => {
+        this.setState({value: event.target.value});
+    }
+
+    handleChange_taille = (event) => {
+        this.setState({taille: event.target.value});
+    }
+    handleChange_nb = (event) => {
+        this.setState({nb: event.target.value});
+    }
+    
+    handleSubmit = (event) => {
+    alert('Le nouvel utilisateur a été soumis : pays : ' + this.state.value +' maison de la taille de ' + this.state.taille +' accueillant '+this.state.taille+' personnes.');
+    event.preventDefault();
+    }
+
+    render(){
+        return(
+            <div className="page_admin">
+                <div className="row justify-content-center">
+                    <h1>Ajouter des données</h1>
+                </div>
+                <div className="row justify-content-center">
+                    <h3>Ajouter un utilisateur</h3>
+                </div> <br></br>
+                <div className="row justify-content-center">
+                    <form className="text-center" onSubmit={this.handleSubmit}>
+
+                        <label>
+                            Insérer le pays : 
+                            <input type="text" placeholder="France" onChange={this.handleChange_nom} name="pais"/>
+                        </label> <br></br><br></br>
+
+
+                        <label>
+                        Quel est la taille de la maison ? <br></br>
+                        <select name="taile" onChange={this.handleChange_taille}>
+                            <option value="large">LARGE</option>
+                            <option value="medium">MEDIUM</option>
+                            <option value="small">SMALL</option>
+                        </select>
+                        </label> <br></br><br></br>
+
+                        <label>
+                            Combien de personne vivent dans la maison ? <br></br>
+                            <input type="number" placeholder="4" name="perso" onChange={this.handleChange_nb}/>personnes
+                        </label> <br></br><br></br> 
+                        
+                        <input type="submit" value="Enregistrer" />
+                    </form>
+
+                </div>
+            </div>
+
+
+        );
+    }
+
+
+}
+
+export default Admin;
+
+
+
--- frontend/src/navigateur.js
+++ frontend/src/navigateur.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Dashboard from './dashboard';
+import Admin from './admin';
 
 class Navigateur extends React.Component{
 
@@ -8,13 +9,12 @@ class Navigateur extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            accueil : "actif"
+            accueil : null
         };
 
     }
 
     click_accueil = () => { 
-        console.log("TRIGGERR");
         this.setState({accueil:"actif"});
     }
 
@@ -23,48 +23,37 @@ class Navigateur extends React.Component{
     }
 
     render(){
-        console.log("this.state", this.state.accueil);
        if(this.state.accueil==="actif"){
         return(
             <div className="container">
             <div className="row justify-content-center">
-            <nav className = "navbar navbar-fixed-top navbar-default" >
-            <ul className="nav navbar-nav">
-                <li>
+            
                     <button className = "accueil" onClick={this.click_accueil}>Accueil </button> 
-                </li>
-                <li>
+                
                     <button className = "admin" onClick={this.click_admin}>Admin </button>
-                </li>                
-            </ul>
-            </nav>
+
             </div>
             <div className = "row justify-content-center">
             <Dashboard />
             </div>    
             </div>
-        )
+        );
        }
        else if(this.state.accueil===null){
            return(
         <div className="container">
         <div className="row justify-content-center">
-        <nav className = "navbar navbar-fixed-top navbar-default" >
-        <ul className="nav navbar-nav">
-            <li>
+        
                 <button className = "accueil" onClick={this.click_accueil}>Accueil </button> 
-            </li>
-            <li>
+         
                 <button className = "admin" onClick={this.click_admin}>Admin </button>
-            </li>                
-        </ul>
-        </nav>
+
         </div>
         <div className = "row justify-content-center">
-        <p>Nous sommes dans la page admin</p>
+        <Admin/>
         </div>    
         </div>
-           )
+           );
        }
     }
 }
